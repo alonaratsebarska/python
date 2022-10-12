618e052c93315ce24ce7efbf04c6958b89be5e5d
Alona Ratsebarska <aleonorarace@gmail.com>
2022-10-13 02:24:56 +0300

    Sem_1.Task_1

--- seminar_1/task_1.py
+++ seminar_1/task_1.py
@@ -6,3 +6,15 @@
 # - 7 -> да
 # - 1 -> нет
 
+num = (int(input('Vvedite cyfru, oboznachajushchuju den nedeli: ')))
+
+def DaysOfWeek(number):
+    if number == 6 or number == 7:
+        result = 'Da. Segodnia vychodnoj den. Pozdravliaju!!!'
+    elif number >= 1 and number <= 5:
+        result = 'Net. Segodnia rabochij den. Idi na rabotu / uchebu :) '
+    else:
+        result = 'Vvedite chislo v diapazone ot 1-go do 7-mi'
+    return result
+
+print(DaysOfWeek(num))           
